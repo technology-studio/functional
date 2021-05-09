51a19fe105907af9f10fa1c90329a76b7793332c
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2021-05-09 13:20:53 +0200

    Add error callbacks on onlyOne, atLeastOne and upgrade outdated dependencies

--- packages/@txo/functional/src/Api/Array.ts
+++ packages/@txo/functional/src/Api/Array.ts
@@ -20,16 +20,16 @@ export const findById = <ENTITY extends{ id: string }>(
   return id ? entityList.find(entity => entity.id === id) : null
 }
 
-export const onlyOne = <TYPE>(list: TYPE[]): TYPE => {
+export const onlyOne = <TYPE>(list: TYPE[], errorCallback?: () => Error): TYPE => {
   if (list.length !== 1) {
-    throw new Error(`Expected only one item in array (actual: ${list.length}).`)
+    throw errorCallback ? errorCallback() : new Error(`Expected only one item in array (actual: ${list.length}).`)
   }
   return list[0]
 }
 
-export const atMostOne = <TYPE>(list: TYPE[]): TYPE | null => {
+export const atMostOne = <TYPE>(list: TYPE[], errorCallback?: () => Error): TYPE | null => {
   if (list.length > 1) {
-    throw new Error(`Expected zero or one item in array (actual: ${list.length}).`)
+    throw errorCallback ? errorCallback() : new Error(`Expected zero or one item in array (actual: ${list.length}).`)
   }
   if (list.length === 1) {
     return list[0]
